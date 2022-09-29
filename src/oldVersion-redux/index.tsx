3f909a02ea4dd4670447bbf9ba347f35ab9cf89c
王巨仙 <2916709093@qq.com>
2022-09-29 23:00:55 +0800

    feat: update immer

--- src/oldVersion-redux/index.tsx
+++ src/oldVersion-redux/index.tsx
@@ -2,7 +2,7 @@
  * @Author: 王荣
  * @Date: 2022-09-26 13:44:42
  * @LastEditors: 王荣
- * @LastEditTime: 2022-09-26 23:06:02
+ * @LastEditTime: 2022-09-29 22:52:45
  * @Description: 填写简介
  */
 
@@ -10,8 +10,8 @@ import { Provider } from "react-redux";
 
 import { default as TodoListThunk } from "./origin-redux-thunk/TodoList";
 import { default as TodoListSaga } from "./origin-redux-saga/TodoList";
-import { default as storeClass } from "./redux-class/store";
-import { default as TodoListClass } from "./redux-class/app/TodoList";
+import { default as storeClass } from "./redux-class-immer/store";
+import { default as TodoListClass } from "./redux-class-immer/app/TodoList";
 import { default as storeImmutable } from "./redux-class-immutable/store";
 import { default as TodoListImmutable } from "./redux-class-immutable/app/TodoList";
 
